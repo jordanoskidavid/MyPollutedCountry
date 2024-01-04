3a264840db46f445f99dbb8b9dea415a5f916ca0
David Jordanoski <jordanoski.david16@gmail.com>
2024-01-04 01:16:15 +0100

    Add the interaction map API with small customization

---
+++ script.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([41.6086, 21.7453],8.4);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
